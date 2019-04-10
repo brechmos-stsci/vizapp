cbc5cacda141d0b0898cfd2d80a549cbba388cb2
Craig Jones <craig@brechmos.org>
2019-04-10 16:35:11 -0400

    fixed overlays, abstracted NDViewer more

--- vizapp/viewers/viewernd.py
+++ vizapp/viewers/viewernd.py
@@ -1,9 +1,17 @@
+import logging
+
 import numpy as np
 import plotly.graph_objs as go
-from ipywidgets import IntSlider, Dropdown, HBox, VBox, Label, FloatText, Button
+from ipywidgets import IntSlider, Dropdown, HBox, VBox, Label, FloatText, Button, IntText
 
 from .viewer import Viewer
 
+logging.basicConfig(filename='/tmp/vizapp.log',
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
+logger = logging.getLogger('viewernd')
 
 class ViewerND(Viewer):
 
@@ -80,8 +88,12 @@ class ViewerND(Viewer):
 
         """
 
+        logger.debug('overlay_dropdown_on_change with change {}'.format(change))
         if change['type'] == 'change' and change['name'] == 'value':
             self._theoverlay = self._vizapp.get_data(change['new'])
+
+            logger.debug('\tgoing to call update image')
+            self._update_image()
 
     def _processing_dropdown_on_change(self, change):
         """
@@ -121,12 +133,15 @@ class ViewerND(Viewer):
 
         # Add all the parameters
         for parameter in self._processing_parameters.get('parameters'):
-            if isinstance(parameter[1], float):
+            if isinstance(parameter[1], (int, float)):
 
                 label = Label(parameter[0])
                 label.layout.width = '50%'
 
-                ft = FloatText(value=parameter[1])
+                if isinstance(parameter[1], int):
+                    ft = IntText(value=parameter[1])
+                elif isinstance(parameter[1], float):
+                    ft = FloatText(value=parameter[1])
                 ft.layout.width = '50%'
 
                 box = HBox((label, ft ))
@@ -150,14 +165,45 @@ class ViewerND(Viewer):
         self._processing_vbox.children = ()
 
     def _processing_process_button_callback(self, *args, **kwargs):
+        logger.debug('-------------------')
+        logger.debug(*args)
 
         # Get the data
         data_name = self._data_dropdown.value
         data = self._vizapp.get_data(data_name)
 
-        new_data = self._processing_parameters['method'](data,
-                                                         self._processing_parameters['parameters'][1][1])
+        # Get the function
+        function = self._processing_parameters['method']
 
+        # Need to see which parameters need to be passed to the function
+        params = {
+            self._processing_parameters['data_parameter']: data
+        }
+        for ii, row in enumerate(self._processing_vbox.children):
+            logger.debug('{}th row is {}'.format(ii, row))
+            if hasattr(row, 'children') and len(row.children) == 2 and isinstance(row.children[1], FloatText):
+                label, box = row.children
+                logger.debug('label is {}'.format(label))
+                logger.debug('box is {}'.format(box))
+                params[label.value] = box.value
+            elif hasattr(row, 'children') and len(row.children) == 2 and isinstance(row.children[1], IntText):
+                label, box = row.children
+                logger.debug('label is {}'.format(label))
+                logger.debug('box is {}'.format(box))
+                params[label.value] = box.value
+
+        logger.debug('Got parameters {}'.format(params))
+
+
+        logger.debug('_processing_process_button_callback: going to call {} with {}'.format(
+            function,
+            params
+        ))
+        new_data = function(**params)
+
+        logger.debug('Data returned from {} with params {} is {}'.format(function, params, new_data))
+
+        # Add the new data to vizapp
         self._vizapp.add_data(data_name + '-' + self._processing_parameters['name'], new_data)
 
         # Reset the GUI
@@ -165,8 +211,7 @@ class ViewerND(Viewer):
         self._processing_vbox.children = ()
 
         self._data_dropdown.options = self._vizapp._3d_data.keys()
-
-
+        self._overlay_dropdown.options = ['None'] + list(self._vizapp._2d_data.keys())
 
     def _slice_slider_on_value_change(self, change):
         """
@@ -228,6 +273,25 @@ class PlotlyViewerND(ViewerND):
         td = self._thedata[self._current_slice]
         self._fig.data[0].update({'z': td})
 
+        if not self._overlay_dropdown.value == 'None':
+            self._fig.data[1].update({
+                "x": np.arange(74),
+                "y": np.arange(74),
+                "z": self._theoverlay,
+                "opacity": 0.5,
+                "showlegend": False,
+                "type": "heatmap"
+            })
+        else:
+            self._fig.data[1].update({
+                "x": [0],
+                "y": [0],
+                "z": [0],
+                "opacity": 1,
+                "showlegend": False,
+                "type": "heatmap"
+            })
+
     def _show_image(self):
 
         self._current_slice = 0
@@ -235,6 +299,7 @@ class PlotlyViewerND(ViewerND):
         colorscale = [(x, 'rgb({}, {}, {})'.format(int(x*255), int(x*255), int(x*255))) for x in np.arange(0, 1, 0.1)]
 
         self._trace1 = {
+            "name": "data",
             "x": np.arange(74),
             "y": np.arange(74),
             "z": self._thedata[0],
@@ -242,16 +307,28 @@ class PlotlyViewerND(ViewerND):
             "showlegend": False,
             "type": "heatmap"
         }
-        self._data = go.Data([self._trace1])
-        #        self._trace2 = {
-        #            "x": np.arange(74),
-        #            "y": np.arange(74),
-        #            "z": self._theoverlay[0],
-        #            "opacity": 0.3,
-        #            "showlegend": False,
-        #            "type": "heatmap"
-        #        }
-        #        self._data = go.Data([self._trace1, self._trace2])
+
+        data2show = [self._trace1]
+
+        # logger.debug('Going to show with overlay {}'.format(self._overlay_dropdown.value))
+        # if not self._overlay_dropdown.value == 'None':
+        #     name = self._overlay_dropdown.value
+        #     data = self._vizapp._2d_data[name]
+        #
+        overlay_data = {
+            "name": "overlay",
+            "x": [0],
+            "y": [0],
+            "z": [0],
+            "opacity": 1,
+            "colorscale": colorscale,
+            "showlegend": False,
+            "type": "heatmap"
+        }
+
+        data2show += [overlay_data]
+
+        self._data = go.Data(data2show)
 
         layout = {
             "legend": {
