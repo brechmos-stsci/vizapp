40a8fdd9cf6f71710c7898323c8aecf300d1e521
Craig Jones <craig@brechmos.org>
2019-04-11 15:02:36 -0400

    overlay with contours, 1D plot switch

--- vizapp/vizapp.py
+++ vizapp/vizapp.py
@@ -2,6 +2,7 @@ import inspect
 import logging
 
 import numpy as np
+import scipy.signal
 
 logging.basicConfig(filename='/tmp/vizapp.log',
                             filemode='a',
@@ -22,12 +23,19 @@ class VizApp:
         self._1d_data = {}
 
         self._3d_processing = {}
-        self.add_3d_processing("Median Collapse", np.nanmedian, 'a', (('axis', 0),))
-        self.add_3d_processing("Mean Collapse", np.nanmean, 'a', (('axis', 0),))
+        self.add_3d_processing("Median Collapse over Wavelenths", np.nanmedian, 'a', (('axis', 0),))
+        self.add_3d_processing("Mean Collapse over Wavelenths", np.nanmean, 'a', (('axis', 0),))
+        self.add_3d_processing("Median Collapse over Space", np.nanmedian, 'a', (('axis', (1,2)),))
+        self.add_3d_processing("Mean Collapse over Space", np.nanmean, 'a', (('axis', (1,2)),))
 
         self._2d_processing = {}
 
         self._1d_processing = {}
+        self.add_1d_processing("Median Smoothing", scipy.signal.medfilt, 'volume', (('kernel_size', 3),))
+        self.add_1d_processing("Hanning Smoothing", np.convolve, 'a', (('mode', 'valid'), ('w', np.hanning(3))))
+        self.add_1d_processing("Hamming Smoothing", np.convolve, 'a', (('mode', 'valid'), ('w', np.hamming(3))))
+        self.add_1d_processing("Bartlett Smoothing", np.convolve, 'a', (('mode', 'valid'), ('w', np.bartlett(3))))
+        self.add_1d_processing("Blackman Smoothing", np.convolve, 'a', (('mode', 'valid'), ('w', np.blackman(3))))
 
     # ---------------------------------------------------------------
     #
@@ -61,6 +69,9 @@ class VizApp:
             if any([not isinstance(x, tuple) or not len(x) in [0,2] for x in parameters]):
                 raise TypeError('add_3d_processing: each parameter must be a parameter name and default value')
 
+        if name in self._3d_processing:
+            logger.warning('Replacing {} in the 3D processing'.format(name))
+
         self._3d_processing[name] = {
             'name': name,
             'method': func,
@@ -91,11 +102,55 @@ class VizApp:
     def get_2d_processing(self):
         return self._2d_processing
 
-    def add_1d_processing(self, name, func):
-        self._1d_processing[name] = func
+    def add_1d_processing(self, name, func, data_parameter, parameters):
+        """
 
-    def get_1d_processing(self):
-        return self._1d_processing
+        :param name: str name to display
+        :param func: method  method to run
+        :param parameters: tuple - list of parameters
+        :return: none
+        """
+        logger.debug('parameters is {}'.format(parameters))
+
+        if not isinstance(name, str):
+            raise TypeError('add_3d_processing: name, {}, must be a string')
+
+        if not inspect.isfunction(func):
+            raise TypeError('add_3d_processing: func must be a method')
+
+        if not isinstance(data_parameter, str):
+            raise TypeError('add_3d_processing: data_parameter, {}, must be a string')
+
+        if not isinstance(parameters, (list, tuple)):
+            raise TypeError('add_3d_processing: parameters must be a list')
+
+        if parameters:
+            if any([not isinstance(x, tuple) or not len(x) in [0,2] for x in parameters]):
+                raise TypeError('add_3d_processing: each parameter must be a parameter name and default value')
+
+        if name in self._1d_processing:
+            logger.warning('Replacing {} in the 3D processing'.format(name))
+
+        self._1d_processing[name] = {
+            'name': name,
+            'method': func,
+            'data_parameter': data_parameter,
+            'parameters': parameters
+        }
+
+
+    def get_1d_processing(self, name=None):
+        """
+        Get the list of types of processing that can be done on a 3D dataset
+
+        :return: list - list of keys to the types of processing
+        """
+        # TODO: Fix the above description
+
+        if name is not None:
+            return self._1d_processing[name]
+        else:
+            return list(self._1d_processing.keys())
 
     # ---------------------------------------------------------------
     #
@@ -116,6 +171,8 @@ class VizApp:
             self._3d_data[name] = data
         if len(data.shape) == 2:
             self._2d_data[name] = data
+        if len(data.shape) == 1:
+            self._1d_data[name] = data
 
     def get_data(self, name):
         """
@@ -133,10 +190,11 @@ class VizApp:
 
             return self._3d_data[key]
         elif isinstance(name, str):
-
             if name in self._3d_data:
-               return self._3d_data[name]
+                return self._3d_data[name]
             elif name in self._2d_data:
-                return self._2d_data[name]
+             return self._2d_data[name]
+            elif name in self._1d_data:
+                return self._1d_data[name]
         else:
             raise('get_data takes an int or string.')
